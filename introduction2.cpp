ac2d84b1e676f819fb3ddadf95407faa192a10a3
pedro oste <pedstarpedstar@yahoo.com.au>
2021-03-17 19:56:46 +1100

    updated intro2 with lecture 3 content

--- introduction2.cpp
+++ introduction2.cpp
@@ -4,12 +4,48 @@
 
 //function declarations
 void comparingStrings();
+void passingByValue(int a);
+void passingByReference(int &a);
+void basicPointers();
+void functionPointer(void (*function)() );
+void example1();
+void example2();
+void voidPointers();
+void structs();
+void dynamicMemory();
 
 
 // main functions
 int main(){
 
+    basicPointers();
+
     comparingStrings();
+
+    //passing by value vs reference. try to avoid reference and instead return a type.
+    int exampleValue = 5;
+    std::cout << "Here is the example value originally: " << exampleValue << std::endl;
+    passingByValue(exampleValue);
+    std::cout << "Here is the example after passing by value: " << exampleValue << std::endl;
+    passingByReference(exampleValue);
+    std::cout << "Here is the example after passing by reference: " << exampleValue << std::endl;
+
+
+    //now we will send a function as a parameter through pointers
+    std::cout << "Here we will view pointer functions" << std::endl;
+    
+    functionPointer(&example1);
+    functionPointer(&example2);
+
+
+    //now we will use structs
+    std::cout << "Using structs" << std::endl;
+    structs();
+
+    //dynamic memory
+    dynamicMemory();
+    
+
 
     return 0;
 }
@@ -20,7 +56,7 @@ int main(){
 //comparing strings
 void comparingStrings(){
 
-    std::cout << "Please enter: 'whats up man' " << std::endl;
+    std::cout << "Please enter: 'whats up man': ";
     
     std::string userInput;
 
@@ -45,15 +81,165 @@ void comparingStrings(){
 
     //checking for the letter x
     if(userInput.find_first_of("x") == -1){
-        std::cout << "X was not found" << std::endl;
+        std::cout << "x was not found\n" << std::endl;
     }
     else{
-        std::cout << "X was found!!" << std::endl;
+        std::cout << "x was found!!\n" << std::endl;
     }
     
 
 
 }
+
+void passingByValue(int a){
+    //passing by value, the varaible will become local
+    a = 10;
+    std::cout << "(pass by value) The value of the integer within the function: " << a << std::endl;
+}
+
+void passingByReference(int &a){
+    //passing by reference, the variable will become global between the two functions
+    a = 11;
+    std::cout << "(pass by reference) The value of the integer within the function: " << a << std::endl;
+}
+
+void basicPointers(){
+    //basic introduction to pointers, this similar to passing by referenece, however they can be reassigned and perform arthimatic operations
+    //pointers are described by the * operand.
+    int y = 5;
+    int *x = &y;         //this is a pointer to the memory to y
+    *x = 1000;       //this will change for both x and y
+    std::cout << "The value of y is now 1000 as this has been changed by the x pointer. y = " << y << std::endl;
+}
+
+void functionPointer(void (*function)() ){
+    //demonstrating the use of function pointers
+
+    /* note that we can also use pointers to point to the memory address of functions
+    . this could be usefull in situation such as passing functions as a parameter 
+    */
+
+   function();
+
+}
+
+void example1(){
+    //basic function used for pointer example
+
+    std::cout << "You are now in the example 1 function" << std::endl;
+
+}
+
+void example2(){
+    //basic function used for pointer example
+
+    std::cout << "You are now in the example 2 function" << std::endl;
+
+}
+
+void voidPointers(){
+    //we can use void pointers to point to a variety of data types
+    int integer = 5;
+    void *vp;
+    vp = &integer;
+
+    //note that we have to cast the void function in order to heap
+    std::cout << "Here is the void pointer initially: " << *((int*)vp ) << std::endl;
+
+    char character = 'A';
+    vp = &character;
+
+    std::cout << "Here is the void pointer now: " << *((char*)vp ) << std::endl;
+
+
+}
+
+void structs(){
+    //defines the use of structs
+
+
+    /*
+    . Structs are very similar to classes, composing of a set of 
+    public attributes and functions.
+
+    This data has no encapsulation and can be easily accessed and modified
+
+    strucutres must be define in the order that you use them
+    */
+
+    struct Address {
+        std::string city;
+        int postcode;
+    };
+
+    struct Student {
+        const std::string uni = "UWO";
+        std::string name;
+        int id;
+        Address addr;
+    };
+
+    Student s1;
+    s1.addr.postcode = 2500;
+    s1.addr.city = "Mount Keira";
+    s1.id = 679883;
+    s1.name = "Pedro";
+
+    std::cout << s1.name << " is from " << s1.addr.city << " " << s1.addr.postcode << " and is studying at " << s1.uni << std::endl;
+
+
+
+}
+
+void dynamicMemory(){
+    //this function will describe the uses of dynamic memory within cpp
+
+    /*
+    . Dynamic memory is used when we dont know how much memory we need or the type. this is pretty much a form of unassigned pointer
+    . Whilst using DM we must ensure that we delete (release) any memory that we allocate as this can lead to a memory leak
+    . Memory leaks clog up the users memory after the process has been completed (not terminated) and can also lead to information leaks
+    */
+
+   //creating a new pointer
+   int *intptr;
+   //dynamically allocating memory
+   intptr = new int;
+   //note that you must delete the pointer in order to release the memory
+   delete intptr;
+
+   //the auto type can also be used
+   auto *autptr = new auto(5.6);
+   std::cout << "printing out allocated memory: " << *autptr << std::endl;
+   delete autptr;
+
+
+    /*
+    You can also allocate DM to arrays and pointers to pointers, however it is important that we delete all associated pointers. 
+    Imporper use of DM commonly leads to two different types of problems
+
+    Segmentation faults
+    Using memory that does not belong to you, this can be get/set values from an illegal address or out of bounds array references
+
+    Bus faults
+    Accessing memory that that cannot be phyiscally addressed, usually caused by converting a bigger data tpye into a smaller.
+    This causes a sort of memory misalignmnet
+    */
+
+    float **fptr;
+    fptr = new float* [10];   //allocating to array
+
+    for (int i = 0; i < 10; i++){
+        fptr[i] = new float[10]; //allocating memory to each
+    }
+
+    for (int i = 0; i < 10; i++){
+        delete [] fptr[i];      //deleting memory to each
+    }
+
+    delete [] fptr;
+
+}
+
 
 
 
