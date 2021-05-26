1fc959dca50ecebc57f9aaaf93ebfdc07405ac3a
pedro oste <pedstarpedstar@yahoo.com.au>
2021-05-26 18:47:48 +1000

    Lecture 12 P2

---
+++ Lecture 12/DataMining.cpp
@@ -0,0 +1,28 @@
+/*
+Data Mining
+Exploring data through machine based technologies with the goal of discovering knowledge within the data
+
+Machine learning
+aims to equip computers with human like abilities, such as learning!
+
+Predicitive tasks
+    To make a prediction about the data, such as data classification, regression and time series analysis
+    Classification
+        Determines the attributes of data in order to place it into pre defined classifications. This is commonly
+        seen within decision trees and neural networks.
+
+        Other popular methods include linear classifiers and k-nearest neighbors (using other classified examples to classify yourself).
+
+Descriptive tasks
+    Describes the data characteristics, clustering, association rules, sequence discovery
+    Clustering
+        groups of similar data into clusters, such as determining what movies the user would like to watch.
+
+Python vs C++?
+    cpp is beneficial when wanting to control the hardware, they also have many libraries where machine learning has been implemented.
+    Due to the compiler nature of cpp it is also faster than python
+    However python has more community support and is easier to use as you dont need the level of depth in relation to dealing with hardware.
+
+    Cpp is used within many applications that deal with systems at a hardware level, such as IOT and smart devices. It can also be used within in
+    depth scenarios such as multi-thread and multi-process programming
+*/
